4c579b8937ae66c1d28af29a6a6822f775149f85
sbrinnich-tgm <sbrinnich@student.tgm.ac.at>
2015-11-08 14:47:59 +0100

    Merge

--- src_evaluation/panda3d/rotating_sphere.py
+++ src_evaluation/panda3d/rotating_sphere.py
@@ -9,21 +9,22 @@ class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-        # Load the sphere model.
+        # Models laden
         self.sphere = self.loader.loadModel("models/sphere")
-        self.sphere2 = self.loader.loadModel("models/purplesphere")
-        # Reparent the model to render.
+        self.sphere2 = self.loader.loadModel("models/sphere")
+        # Models zu render reparenten
         self.sphere2root = self.render.attachNewNode('sphere2root')
         self.sphere.reparentTo(self.render)
         self.sphere2.reparentTo(self.sphere2root)
-        # Apply scale and position transforms on the model.
+        # Scale und Position Transformationen auf Models anwenden
         self.sphere.setScale(2, 2, 2)
         self.sphere.setPos(0, 0, 0)
         self.sphere2.setScale(1, 1, 1)
         self.sphere2.setPos(10, 0, 0)
+        # Kamera setzen
         self.trackball.node().setPos(0,40,0)
-
-        self.rotateSphere2 = self.sphere2root.hprInterval((0.241 * 10), Vec3(360, 0, 0))
+        # Sphere2 um Sphere rotieren lassen
+        self.rotateSphere2 = self.sphere2root.hprInterval(3, Vec3(360, 0, 0))
         self.rotateSphere2.loop()
 
 app = MyApp()
