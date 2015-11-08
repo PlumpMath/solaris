9f6a84b115814d1193bfaf2883caff869c818d54
sbrinnich-tgm <sbrinnich@student.tgm.ac.at>
2015-11-08 14:01:37 +0100

    Changed models

--- src_evaluation/panda3d/rotating_sphere.py
+++ src_evaluation/panda3d/rotating_sphere.py
@@ -1,4 +1,8 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from panda3d.core import Vec3
 
 class MyApp(ShowBase):
 
@@ -6,12 +10,21 @@ class MyApp(ShowBase):
         ShowBase.__init__(self)
 
         # Load the sphere model.
-        self.environ = self.loader.loadModel("models/sphere")
+        self.sphere = self.loader.loadModel("models/sphere")
+        self.sphere2 = self.loader.loadModel("models/purplesphere")
         # Reparent the model to render.
-        self.environ.reparentTo(self.render)
+        self.sphere2root = self.render.attachNewNode('sphere2root')
+        self.sphere.reparentTo(self.render)
+        self.sphere2.reparentTo(self.sphere2root)
         # Apply scale and position transforms on the model.
-        self.environ.setScale(2, 2, 2)
-        self.environ.setPos(0, 0, 0)
+        self.sphere.setScale(2, 2, 2)
+        self.sphere.setPos(0, 0, 0)
+        self.sphere2.setScale(1, 1, 1)
+        self.sphere2.setPos(10, 0, 0)
+        self.trackball.node().setPos(0,40,0)
+
+        self.rotateSphere2 = self.sphere2root.hprInterval((0.241 * 10), Vec3(360, 0, 0))
+        self.rotateSphere2.loop()
 
 app = MyApp()
 app.run()
