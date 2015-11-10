196163935ee555f17f5c2e5c995dceab15040cbc
sbrinnich-tgm <sbrinnich@student.tgm.ac.at>
2015-11-10 15:21:41 +0100

    Added feature to pause, resume and toggle speed of animation

--- src/solarsystem.py
+++ src/solarsystem.py
@@ -6,9 +6,11 @@ from panda3d.core import *
 
 from universe import Planet, Moon, Sky
 
+import sys
+
 class Solaris(ShowBase):
 
-    speed = 1
+    speed = 0.1
     animRunning = True
 
     def __init__(self):
@@ -20,44 +22,95 @@ class Solaris(ShowBase):
         self.setBackgroundColor(0, 0, 0)
 
         # Models laden
-        self.sky = Sky(app=self, model="models/solar_sky_sphere", texture="models/stars_1k_tex.jpg", scale=100)
+        self.sky = Sky(app=self, model="models/solar_sky_sphere", texture="models/stars.jpg", scale=1000)
         self.sky.create_sky()
 
-        self.sun = Planet(app=self, name="Sun", model="models/planet_sphere", texture="models/sun_1k_tex.jpg", scale=2, distance_from_sun=0)
+        self.sun = Planet(app=self, name="Sun", model="models/planet_sphere", texture="models/sun.jpg", scale=20, distance_from_sun=0, speed_self=10)
         self = self.sun.create_planet()
         self.sun.rotate_planet()
 
-        self.earth = Planet(app=self, name="Earth", model="models/planet_sphere", texture="models/earth_1k_tex.jpg", scale=1, distance_from_sun=10)
+        self.mercury = Planet(app=self, name="Mercury", model="models/planet_sphere", texture="models/mercury.jpg", scale=1.6, distance_from_sun=30, speed_self=10, speed=100)
+        self = self.mercury.create_planet()
+        self.mercury.rotate_planet()
+
+        self.venus = Planet(app=self, name="Venus", model="models/planet_sphere", texture="models/venus.jpg", scale=2.7, distance_from_sun=40, speed_self=2, speed=80)
+        self = self.venus.create_planet()
+        self.venus.rotate_planet()
+
+        self.earth = Planet(app=self, name="Earth", model="models/planet_sphere", texture="models/earth.jpg", scale=2.8, distance_from_sun=50, speed_self=15, speed=70)
         self = self.earth.create_planet()
         self.earth.rotate_planet()
 
-        self.moon = Moon(app=self, planet=self.earth, name="Earth Moon", model="models/planet_sphere", texture="models/moon_1k_tex.jpg", scale=0.2, distance_from_planet=2)
+        self.moon = Moon(app=self, planet=self.earth, name="Earth Moon", model="models/planet_sphere", texture="models/moon.jpg", scale=0.1, distance_from_planet=1)
         self.moon.create_moon()
         self.moon.rotate_moon()
 
-        self.mars = Planet(app=self, name="Mars", model="models/planet_sphere", texture="models/mars_1k_tex.jpg", scale=1, distance_from_sun=20)
+        self.mars = Planet(app=self, name="Mars", model="models/planet_sphere", texture="models/mars.jpg", scale=1.9, distance_from_sun=57, speed_self=16, speed=60)
         self = self.mars.create_planet()
         self.mars.rotate_planet()
+
+        self.jupiter = Planet(app=self, name="Jupiter", model="models/planet_sphere", texture="models/jupiter.jpg", scale=12, distance_from_sun=85, speed_self=18, speed=40)
+        self = self.jupiter.create_planet()
+        self.jupiter.rotate_planet()
+
+        self.saturn = Planet(app=self, name="Saturn", model="models/planet_sphere", texture="models/saturn.jpg", scale=10, distance_from_sun=115, speed_self=17, speed=30)
+        self = self.saturn.create_planet()
+        self.saturn.rotate_planet()
+
+        self.uranus = Planet(app=self, name="Uranus", model="models/planet_sphere", texture="models/uranus.jpg", scale=6.7, distance_from_sun=145, speed_self=13, speed=20)
+        self = self.uranus.create_planet()
+        self.uranus.rotate_planet()
+
+        self.neptune = Planet(app=self, name="Neptune", model="models/planet_sphere", texture="models/neptune.jpg", scale=6.5, distance_from_sun=185, speed_self=12, speed=10)
+        self = self.neptune.create_planet()
+        self.neptune.rotate_planet()
 
         self.ambLight = AmbientLight("ambientlight")
         self.ambLight.setColor(Vec4(0.2, 0.1, 0.1, 1.0))
         self.ambNode = self.render.attachNewNode(self.ambLight)
-        self.render.setLight(self.ambNode)
+        self.mercury.get_planet_node().setLight(self.ambNode)
+        self.venus.get_planet_node().setLight(self.ambNode)
+        self.earth.get_planet_node().setLight(self.ambNode)
+        self.moon.get_moon_node().setLight(self.ambNode)
+        self.mars.get_planet_node().setLight(self.ambNode)
+        self.jupiter.get_planet_node().setLight(self.ambNode)
+        self.saturn.get_planet_node().setLight(self.ambNode)
+        self.uranus.get_planet_node().setLight(self.ambNode)
+        self.neptune.get_planet_node().setLight(self.ambNode)
 
         plight = PointLight('pointlight')
         self.pointNode = self.sun.get_planet_node().attachNewNode(plight)
         self.pointNode.setPos(0, 0, 0)
-        self.render.setLight(self.pointNode)
+        self.mercury.get_planet_node().setLight(self.pointNode)
+        self.venus.get_planet_node().setLight(self.pointNode)
+        self.earth.get_planet_node().setLight(self.pointNode)
+        self.moon.get_moon_node().setLight(self.pointNode)
+        self.mars.get_planet_node().setLight(self.pointNode)
+        self.jupiter.get_planet_node().setLight(self.pointNode)
+        self.saturn.get_planet_node().setLight(self.pointNode)
+        self.uranus.get_planet_node().setLight(self.pointNode)
+        self.neptune.get_planet_node().setLight(self.pointNode)
 
         self.render.setShaderAuto()
+
+        self.accept("escape", sys.exit)
+        self.accept("+", self.speed_change, ['+'])
+        self.accept("-", self.speed_change, ['-'])
+        self.accept("space", self.stopPlaySimulation)
 
         # Kamera setzen
         #self.trackball.node().setPos(0,40,0)
         #self.camera.setPos(0,-40,0)
         # self.mouseLook = FirstPersonCamera(self, self.cam, self.render)
 
-    def get_speed(self):
-        return self.speed
+    def speed_change(self,plus_minus):
+        if plus_minus == '+' and self.speed < 1:
+            self.speed += 0.1
+        elif plus_minus == '-' and self.speed > 0.1:
+            self.speed -= 0.1
+
+    def stopPlaySimulation(self):
+        self.animRunning = not self.animRunning
 
     def get_lightnode(self):
         return self.dirNode
--- src/universe.py
+++ src/universe.py
@@ -5,13 +5,17 @@ from math import cos, sin, pi
 
 class Planet():
 
-    def __init__(self, app, name, model, texture=None, scale=1, distance_from_sun=0):
+    def __init__(self, app, name, model, texture=None, scale=1, distance_from_sun=0, speed_self=1, speed=1):
         self.app = app
         self.name = name
         self.model = model
         self.texture = texture
         self.scale = scale
         self.distance_from_sun = distance_from_sun
+        self.speed_self = speed_self
+        self.speed = speed
+        self.angle = self.speed * self.app.speed
+        self.rotation = self.speed * self.app.speed * self.speed_self
 
     def create_planet(self):
         self.planet = self.app.loader.loadModel(self.model)
@@ -28,11 +32,13 @@ class Planet():
         self.app.taskMgr.add(self.rotateTask, self.name+"RotateTask")
 
     def rotateTask(self, task):
-        if self.distance_from_sun != 0:
-            angleDegrees = task.time * self.app.get_speed()*8
-            angleRadians = angleDegrees * (pi / 180.0)
-            self.planet.setPos(self.distance_from_sun * cos(angleRadians), (self.distance_from_sun+4) * sin(angleRadians), self.root.getZ())
-        self.planet.setHpr(task.time*self.app.get_speed()*16, 0, 0)
+        if self.app.animRunning:
+            if self.distance_from_sun != 0:
+                self.angle = self.angle + (self.speed * self.app.speed/10)
+                angleRadians = self.angle * (pi / 180.0)
+                self.planet.setPos(self.distance_from_sun * cos(angleRadians), (self.distance_from_sun+4) * sin(angleRadians), self.root.getZ())
+            self.rotation = self.rotation + (self.speed*self.app.speed/10*self.speed_self)
+            self.planet.setHpr(self.rotation, 0, 0)
         return Task.cont
 
     def get_planet_node(self):
@@ -64,11 +70,14 @@ class Moon():
         self.app.taskMgr.add(self.rotateTask, self.name+"RotateTask")
 
     def rotateTask(self, task):
-        angleDegrees = task.time * self.app.get_speed()*8
+        angleDegrees = task.time * self.app.speed*8
         angleRadians = angleDegrees * (pi / 180.0)
         self.moon.setPos((self.distance_from_planet+0.3) * cos(angleRadians), (self.distance_from_planet+0.8) * sin(angleRadians), self.root.getZ())
-        self.moon.setHpr(task.time*self.app.get_speed()*16, 0, 0)
+        self.moon.setHpr(task.time*self.app.speed*16, 0, 0)
         return Task.cont
+
+    def get_moon_node(self):
+        return self.moon
 
 class Sky():
     def __init__(self, app, model, texture=None, scale=1):
